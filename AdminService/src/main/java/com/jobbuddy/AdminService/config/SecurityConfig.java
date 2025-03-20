c24b35bc31b0adee90a3f8f43657d36d6d2758ad
mohith4115 <mohithnc456@gmail.com>
2025-03-20 22:42:39 +0530

    added security config to adminservice

--- AdminService/src/main/java/com/jobbuddy/AdminService/config/SecurityConfig.java
+++ AdminService/src/main/java/com/jobbuddy/AdminService/config/SecurityConfig.java
@@ -1,11 +1,10 @@
-package com.jobbuddy.UserService.config;
+package com.jobbuddy.AdminService.config;
 
 import com.jobbuddy.UserService.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -13,7 +12,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
